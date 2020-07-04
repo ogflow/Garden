a06a75398349482551732dff0eb4087e04ac773e
sergey <sergey@mios.fi>
2020-07-04 13:55:43 +0300

    Shit is cleaned out. Let's begin doing business!

---
+++ src/home/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Home.scss'
+import { Footer, Header } from '../common'
+
+const Home = () => (
+  <>
+    <Header/>
+    <HomeScreen/>
+    <Footer/>
+  </>
+)
+
+const HomeScreen = () => {
+
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
