8362945d2ffcd5e12990b220b36b3dc763370fda
sergey <sergey@mios.fi>
2020-07-09 10:58:36 +0300

    Updates

---
+++ precache-manifest.36896c2ffeae3d658eaa7039fcf70eb9.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5188e9d7d4852c86bd9db75b63b51464",
+    "url": "/Garden/index.html"
+  },
+  {
+    "revision": "9e555ea52ce358f7a9ee",
+    "url": "/Garden/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "84081e4b94aac31694b0",
+    "url": "/Garden/static/css/main.e3649a26.chunk.css"
+  },
+  {
+    "revision": "9e555ea52ce358f7a9ee",
+    "url": "/Garden/static/js/2.604c8a79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Garden/static/js/2.604c8a79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84081e4b94aac31694b0",
+    "url": "/Garden/static/js/main.f33bf435.chunk.js"
+  },
+  {
+    "revision": "90dcc7f783a92e110bf9",
+    "url": "/Garden/static/js/runtime-main.25a2e6de.js"
+  },
+  {
+    "revision": "758e53af14929cc1336cec0dc9cf4f65",
+    "url": "/Garden/static/media/abbvie.758e53af.svg"
+  },
+  {
+    "revision": "98a2cc32d213eb9acaca7c41195c67e3",
+    "url": "/Garden/static/media/address.98a2cc32.svg"
+  },
+  {
+    "revision": "eb0e0a04aa1c50e1049ce3efdec79601",
+    "url": "/Garden/static/media/astrazeneca.eb0e0a04.png"
+  },
+  {
+    "revision": "fcd9b7697de802c218b475dd01687dea",
+    "url": "/Garden/static/media/boehringer.fcd9b769.svg"
+  },
+  {
+    "revision": "e962152456739548c697d239f3aaec19",
+    "url": "/Garden/static/media/email.e9621524.svg"
+  },
+  {
+    "revision": "dcbf0cc0a9d267e135787f7fc9b4c6dc",
+    "url": "/Garden/static/media/logotype.dcbf0cc0.svg"
+  },
+  {
+    "revision": "127452fc98474d40e2f87879c73014a2",
+    "url": "/Garden/static/media/phizer.127452fc.svg"
+  },
+  {
+    "revision": "d9f490fae932f83232faec1073b1370f",
+    "url": "/Garden/static/media/phone.d9f490fa.svg"
+  },
+  {
+    "revision": "054e4334551687f6ecdf2611dc020b4d",
+    "url": "/Garden/static/media/send.054e4334.svg"
+  }
+]);
